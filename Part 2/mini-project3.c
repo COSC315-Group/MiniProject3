ec3c7b2d7f3f818db10b385759157f622e4eb26c
ubcbent <bentisserand98@gmail.com>
2020-04-05 13:27:59 -0700

    Finished myfilesystem, ls and started create
    Signed-off-by: ubcbent <bentisserand98@gmail.com>

--- Part 2/mini-project3.c
+++ Part 2/mini-project3.c
@@ -1,11 +1,26 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdint.h>
+
+struct indexNode{
+  char name[8]; //file name
+  int size;     // file size (in number of blocks)
+  int blockPointers[8]; // direct block pointers
+  int used;     // 0 => inode is free; 1 => in use
+};
+
+struct superBlock{
+  int8_t freeBlocks[128];
+  struct indexNode inodes[16];
+};
+
+struct superBlock super;
 
 void myFileSystem(const char* diskName)
 {
    // Open the file with name diskName
-   FILE *input = fopen(diskName,"r");
+   FILE *input = fopen(diskName,"rb");
    if(input == NULL){
      printf("Could not find disk '%s'. Please try again\n",diskName);
      exit(-1);
@@ -15,9 +30,22 @@ void myFileSystem(const char* diskName)
    //	position where a struct/object begins. You can use the sizeof operator to help
    //	cleanly determine the position. Next, cast the pointer to a pointer of the
    //	struct/object type.
+   int8_t freeBlocks[128];
+   fread(freeBlocks, sizeof(int8_t), 128, input);
+   printf("Free block addresses loaded. Current file point: %d\n",ftell(input));
 
+   struct indexNode inodes[16];
+   fread(inodes, sizeof(struct indexNode), 16, input);
+
+   for(int i = 0; i<128; i++){
+     super.freeBlocks[i] = freeBlocks[i];
+   }
+   for(int i = 0; i<16; i++){
+     super.inodes[i] = inodes[i];
+   }
    // Be sure to close the file in a destructor or otherwise before
    // the process exits.
+   fclose(input);
 }
 
 
@@ -30,6 +58,15 @@ int create(char name[8], int size)
   // representing inodes within the super block object.
   // If none exist, then return an error.
   // Also make sure that no other file in use with the same name exists.
+  int inodeIndex = -1;
+  for(int i = 0; i<16; i++){
+    if(inodeIndex == -1 && super.inodes[i].used==0){
+      inodeIndex = i;
+    }
+    if(strcmp(name,super.inodes[i].name)==0){
+      printf("Filename '%s' already in use. Please use a different name or delete the offending file.",name);
+    }
+  }
 
   // Step 2: Look for a number of free blocks equal to the size variable
   // passed to this method. If not enough free blocks exist, then return an error.
@@ -69,6 +106,17 @@ int ls(void)
   // List names of all files on disk
 
   // Step 1: Print the name and size fields of all used inodes.
+  int8_t fileExists = 0;
+  for(int i = 0; i<16; i++){
+    if(super.inodes[i].used==1){super.freeBlocks = &freeBlocks;
+      // inode in use, print info
+      printf("Name: %s\tSize: %d\n",super.inodes[i].name,super.inodes[i].size);
+      fileExists = 1;
+    }
+  }
+  if(fileExists == 0){
+    printf("File system empty.\n");
+  }
 
 } // End ls
 
@@ -100,9 +148,11 @@ int writeBlock(char name[8], int blockNum, char buf[1024])
 } // end write
 
 int main(int argc, char *argv[]){
+  //TODO read the commands from a file
   const char diskName;
   printf("Enter name of disk to load: ");
   scanf("%s",&diskName);
   myFileSystem(&diskName);
+  ls();
 	return 0;
 }
